9cb1103d18ec99c56542dc99ff1177890f9c591c
Moni <moniramoniakhter@gmail.com>
2024-05-01 16:12:38 +0600

    add some changes .gitignore and .env (environment variables)

--- foodrecipe/foodrecipe/settings.py
+++ foodrecipe/foodrecipe/settings.py
@@ -15,12 +15,21 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x*hj_6gkm2xlcu*##(zdm(1+1duqw_kbucc8z5177^2h^dl+&h'
+
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
